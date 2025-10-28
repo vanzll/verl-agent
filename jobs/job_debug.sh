c4734a712af6f7e4003a2f153c048fcb8d327d05
Yu Xingrui <yux5@a2ap-login01.asp2p.nscc.sg>
2025-10-28 20:30:14 +0800

    modified runner files

--- jobs/job_debug.sh
+++ jobs/job_debug.sh
@@ -3,22 +3,22 @@
 # Below, is the queue
 #PBS -q normal
 #PBS -j oe
-#PBS -l select=1:ncpus=56:ngpus=4:mem=512G
-#PBS -l walltime=12:00:00
+#PBS -l select=1:ncpus=8:ngpus=1:mem=16G
+#PBS -l walltime=00:10:00
 #PBS -P 13014347 
-#PBS -N verl-agent_naive_grpo
+#PBS -N verl-agent_grpo
 #PBS -o /home/users/astar/ares/yux5/scratch/verl-agent/log
 # Commands start here
 #cd ${PBS_O_WORKDIR}
 cd /home/users/astar/ares/yux5/scratch/verl-agent
 #cd $PBS_O_WORKDIR || exit $? #Change current directory to submission directory
 nvidia-smi
-#source /home/users/astar/ares/yux5/conda_initialize.sh
 #which python
 #pip install torch torchvision -f https://download.pytorch.org/whl/torch_stable.html --no-cache-dir
 source /home/users/astar/ares/yux5/conda_initialize.sh
 conda activate verl-agent
+pip list
 #python main.py
 #bash ./examples/grpo_trainer/run_alfworld.sh
 #bash ./examples/advanced_grpo_trainer/run_alfworld_advanced_grpo.sh
-bash ./examples/naive_grpo_trainer/run_alfworld_naive_grpo.sh
+#bash ./examples/naive_grpo_trainer/run_alfworld_naive_grpo.sh
