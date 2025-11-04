b952e9bfb381edd2b6d9a0da1a9a70f53f08e012
Yu Xingrui <yux5@a2ap-login02.asp2p.nscc.sg>
2025-11-04 20:30:51 +0800

    update gspo

--- examples/gspo_trainer/run_alfworld_adv_S_loss_S.sh
+++ examples/gspo_trainer/run_alfworld_adv_S_loss_S.sh
@@ -2,25 +2,27 @@ set -x
 ENGINE=${1:-vllm}
 export VLLM_ATTENTION_BACKEND=XFORMERS
 export CUDA_VISIBLE_DEVICES=0,1,2,3
-num_cpus_per_env_worker=0.1 # The CPU resource allocated for each environment worker. If you want to use less CPU resources, you can decrease this value.
+num_cpus_per_env_worker=0.1
 
 train_data_size=16
 val_data_size=128
 group_size=8
+loss_mode="gspo"
+clip_low=0.0003
+clip_high=0.0004
+
+# The CPU resource allocated for each environment worker.
+num_cpus_per_env_worker=0.1
 
 # We only use data preparation to indicate the modality and the data size.
 python3 -m examples.data_preprocess.prepare \
     --mode 'text' \
     --train_data_size $train_data_size \
     --val_data_size $val_data_size
-#actor_rollout_ref.actor.loss_agg_mode='seq-mean-token-sum' means compute the loss in step level (mathematically WIP)
-#algorithm.compute_mean_std_cross_steps=True means compute the advantage in step level
-#algorithm.adv_estimator=advanced_grpo means use the token-level advantage computation
+
 python3 -m verl.trainer.main_ppo \
     algorithm.adv_estimator=grpo \
     +algorithm.compute_mean_std_cross_steps=True \
-    actor_rollout_ref.actor.loss_agg_mode='seq-mean-token-sum' \
-    +algorithm.memory_optimize=False\
     data.train_files=$HOME/data/verl-agent/text/train.parquet \
     data.val_files=$HOME/data/verl-agent/text/test.parquet \
     data.train_batch_size=$train_data_size \
@@ -30,6 +32,7 @@ python3 -m verl.trainer.main_ppo \
     data.filter_overlong_prompts=True \
     data.truncation='error' \
     data.return_raw_chat=True \
+    actor_rollout_ref.actor.policy_loss.loss_mode=$loss_mode \
     actor_rollout_ref.model.path=Qwen/Qwen2.5-1.5B-Instruct \
     actor_rollout_ref.actor.optim.lr=1e-6 \
     actor_rollout_ref.model.use_remove_padding=True \
@@ -54,19 +57,22 @@ python3 -m verl.trainer.main_ppo \
     actor_rollout_ref.ref.fsdp_config.param_offload=True \
     actor_rollout_ref.actor.use_invalid_action_penalty=True \
     actor_rollout_ref.actor.invalid_action_penalty_coef=0.1 \
+    actor_rollout_ref.actor.clip_ratio_low=$clip_low \
+    actor_rollout_ref.actor.clip_ratio_high=$clip_high \
     algorithm.use_kl_in_reward=False \
     env.env_name=alfworld/AlfredTWEnv \
     env.seed=0 \
     env.max_steps=50 \
     env.rollout.n=$group_size \
     env.resources_per_worker.num_cpus=$num_cpus_per_env_worker \
+    trainer.resume_mode='auto' \
     trainer.critic_warmup=0 \
     trainer.logger=['console','wandb'] \
     trainer.project_name='verl_agent_alfworld' \
-    trainer.experiment_name='grpo_qwen2.5_1.5b(A:Step,L:Step)' \
+    trainer.experiment_name='gspo_qwen2.5_1.5b(A:Step, L:Step)' \
     trainer.n_gpus_per_node=4 \
     trainer.nnodes=1 \
     trainer.save_freq=-1 \
     trainer.test_freq=5 \
-    trainer.total_epochs=150 \
+    trainer.total_epochs=200 \
     trainer.val_before_train=True $@
