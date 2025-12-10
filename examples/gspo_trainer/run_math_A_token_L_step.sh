cf556fc619c9ee2977e0fb20b545ee435b615b54
Yu Xingrui <yux5@a2ap-login01.asp2p.nscc.sg>
2025-12-10 13:32:06 +0800

    merged

--- examples/gspo_trainer/run_math_A_token_L_step.sh
+++ examples/gspo_trainer/run_math_A_token_L_step.sh
@@ -24,22 +24,14 @@ python3 -m verl.trainer.main_ppo \
     actor_rollout_ref.actor.optim.lr=1e-6 \
     actor_rollout_ref.model.use_remove_padding=True \
     actor_rollout_ref.actor.ppo_mini_batch_size=256 \
-<<<<<<< HEAD
-    actor_rollout_ref.actor.ppo_micro_batch_size_per_gpu=8 \
-=======
     actor_rollout_ref.actor.ppo_micro_batch_size_per_gpu=4 \
->>>>>>> f53f0f602a9ef10a2b92869fc0ed3b77327cffad
     actor_rollout_ref.actor.use_kl_loss=False \
     actor_rollout_ref.model.enable_gradient_checkpointing=True \
     actor_rollout_ref.actor.fsdp_config.param_offload=False \
     actor_rollout_ref.actor.fsdp_config.optimizer_offload=False \
     actor_rollout_ref.actor.entropy_coeff=0.0 \
     actor_rollout_ref.rollout.log_prob_micro_batch_size_per_gpu=16 \
-<<<<<<< HEAD
-    actor_rollout_ref.rollout.tensor_model_parallel_size=4 \
-=======
     actor_rollout_ref.rollout.tensor_model_parallel_size=1 \
->>>>>>> f53f0f602a9ef10a2b92869fc0ed3b77327cffad
     actor_rollout_ref.rollout.name=vllm \
     actor_rollout_ref.rollout.gpu_memory_utilization=0.5 \
     actor_rollout_ref.rollout.enable_chunked_prefill=False \
